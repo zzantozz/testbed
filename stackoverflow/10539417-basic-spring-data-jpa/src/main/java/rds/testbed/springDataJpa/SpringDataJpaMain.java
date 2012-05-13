21e21ba15ba57abd1c812ad6af94bf9d74a7d14d
Ryan <rds6235@gmail.com>
2012-05-13 02:00:47 -0500

    Extra output in spring data jpa example

--- stackoverflow/10539417-basic-spring-data-jpa/src/main/java/rds/testbed/springDataJpa/SpringDataJpaMain.java
+++ stackoverflow/10539417-basic-spring-data-jpa/src/main/java/rds/testbed/springDataJpa/SpringDataJpaMain.java
@@ -12,9 +12,11 @@ public class SpringDataJpaMain {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/applicationContext.xml");
         ICustomerService service = context.getBean(ICustomerService.class);
+        System.out.println("Create customer 'bob'");
         Customer customerToCreate = new Customer();
         customerToCreate.setFirstName("bob");
         service.createCustomer(customerToCreate);
+        System.out.println("Load bob");
         Customer loaded = service.getCustomer(customerToCreate.getId());
         System.out.println(loaded);
     }
