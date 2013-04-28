cedc9d477be03575200eaba3cb8222dabeb3cd83
Ryan <rds6235@gmail.com>
2013-04-27 23:51:58 -0500

    two new tests showing message processing delays

---
+++ spring-integration-with-activemq-message-groups/src/main/groovy/rds/springintegration/SlowTransformer.groovy
@@ -0,0 +1,24 @@
+package rds.springintegration
+
+import org.slf4j.LoggerFactory
+import org.springframework.integration.Message
+import org.springframework.integration.annotation.Transformer
+import org.springframework.integration.support.MessageBuilder
+/**
+ * Created with IntelliJ IDEA.
+ * User: ryan
+ * Date: 4/27/13
+ * Time: 10:38 AM
+ */
+class SlowTransformer {
+    def log = LoggerFactory.getLogger(SlowTransformer)
+    long delay
+    String name
+
+    @Transformer
+    Message<?> delay(Message<?> message) {
+        log.info("$message.payload at delay transformer $name")
+        sleep delay
+        MessageBuilder.fromMessage(message).setHeader(name, System.currentTimeMillis()).build()
+    }
+}
--- spring-integration-with-activemq-message-groups/src/test/groovy/rds/springintegration/MessageGroupChannelTest.groovy
+++ spring-integration-with-activemq-message-groups/src/test/groovy/rds/springintegration/MessageGroupChannelTest.groovy
@@ -1,13 +1,15 @@
 package rds.springintegration
+
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.ImportResource
+import org.springframework.integration.channel.QueueChannel
 import org.springframework.integration.core.MessagingTemplate
-import org.springframework.integration.core.PollableChannel
 import org.springframework.integration.message.GenericMessage
 import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
+
 /**
  * Created with IntelliJ IDEA.
  * User: ryan
@@ -24,18 +26,63 @@ class MessageGroupChannelTest extends Specification {
     }
 
     @Autowired MessagingTemplate messaging
-    @Autowired PollableChannel outbound
+    @Autowired QueueChannel outbound
+    String randomness = UUID.randomUUID()
 
-    def 'messages go from point A to point B'() {
+    def setup(){
+        outbound.clear()
+    }
+
+    def 'messages go from inbound to outbound'() {
         given:
-        def msg = new GenericMessage('nothing special')
+        def msg = new GenericMessage("nothing special $randomness")
 
         when:
         messaging.send 'inbound', msg
 
         then:
-        def msgOut = outbound.receive(5000)
+        def msgOut = outbound.receive(2000)
         msgOut
-        msgOut.payload == 'nothing special'
+        msgOut.payload == "nothing special $randomness"
+    }
+
+    def 'a single message incurs typical processing delays'() {
+        given:
+        def msg = new GenericMessage("nothing special $randomness")
+
+        when:
+        messaging.send 'inbound', msg
+
+        then:
+        !outbound.receive(1000)
+        def msgOut = outbound.receive(1000)
+        msgOut
+        msgOut.headers.A
+        msgOut.headers.B
+        msgOut.headers.C
+        msgOut.headers.D
+        msgOut.headers.E
+    }
+
+    def 'five messages processed serially shows linear increase in delay'() {
+        given:
+        def messages = multipleMessages(5, randomness)
+
+        when:
+        long start = System.currentTimeMillis()
+        messages.each{ messaging.send 'inbound', it }
+
+        then:
+        long elapsed = System.currentTimeMillis() - start
+        while (outbound.queueSize < 5 && elapsed < 20000) {
+            sleep 50
+            elapsed = System.currentTimeMillis() - start
+        }
+        elapsed > 5 * 1200
+        elapsed < 15000
+    }
+
+    def multipleMessages(int number, String payload) {
+        (1..number).collect{ new GenericMessage("$it $payload") }
     }
 }
